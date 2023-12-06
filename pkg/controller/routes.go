aaa1c4a75a9995299c2565ab7698fa870e475a82
Aparna <aparnasukesh0723@gmail.com>
2023-12-06 17:22:27 +0530

    Route chnaged

--- pkg/controller/routes.go
+++ pkg/controller/routes.go
@@ -10,18 +10,22 @@ func Routes(r *gin.Engine) {
 
 	userGroup := r.Group("/user")
 	{
+		// User - Sign up, Log in
 		userGroup.POST("/register", handler.RegisterUser)
 		userGroup.POST("/register/validate", handler.RegisterValidate)
 		userGroup.POST("/login", handler.UserLogin)
 
+		// User - Products
 		userGroup.GET("/products", handler.GetProducts)
 		userGroup.GET("/product/:id", handler.GetProductByID)
 		userGroup.GET("/product", handler.GetProductByName)
 
+		// User - Brands - Products
 		userGroup.GET("/brands", handler.GetBrandsUser)
 		userGroup.GET("/product/brand/:id", handler.GetProductByBrandID)
 		userGroup.GET("/product/brand", handler.GetProductByBrandName)
 
+		// User - Categories - Products
 		userGroup.GET("/categories", handler.GetCategoriesUser)
 		userGroup.GET("/product/category/:id", handler.GetProductByCategoryID)
 		userGroup.GET("/product/category", handler.GetProductByCategoryName)
@@ -32,21 +36,23 @@ func Routes(r *gin.Engine) {
 	{
 		userAuthGroup.Use(handler.UserAuthRequired)
 
+		// User - Cart
 		userAuthGroup.POST("/cart", handler.AddToCart)
 		userAuthGroup.GET("/cart", handler.CartList)
 		userAuthGroup.DELETE("/cart/:id", handler.DeleteCartItem)
 
+		// User - Profile
 		userAuthGroup.POST("/address", handler.AddAddress)
 		userAuthGroup.GET("/address", handler.ViewAddress)
 
 		userAuthGroup.PATCH("/profile", handler.EditUserProfile)
 		userAuthGroup.GET("/profile/:id", handler.ProfileDetails)
 
+		// User - Order
 		userAuthGroup.GET("/ordersummary", handler.OrderSummary)
 		userAuthGroup.POST("/orderitem", handler.OrderItem)
 		userAuthGroup.GET("/orders", handler.ViewOrders)
 		userAuthGroup.GET("/order/orderid", handler.ViewOrdersByID)
-
 		userAuthGroup.PATCH("order/cancel", handler.OrderCancel)
 
 	}
@@ -79,9 +85,9 @@ func Routes(r *gin.Engine) {
 		adminGroup.PUT("/product/:id", handler.UpdateProduct)
 		adminGroup.DELETE("/product/:id", handler.DeleteProduct)
 
+		// Order Management
 		adminGroup.GET("/orders/:id", handler.ViewOrdersByUserID)
 		adminGroup.GET("/orders/orderid/:id", handler.ViewOrderItemsByUserID)
-
 		adminGroup.PATCH("order/cancel", handler.OrderCancel)
 
 	}
